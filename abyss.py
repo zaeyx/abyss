bbce87671a897283a4808a55abc21d5a21b42ad8
root <root@localhost.localdomain>
2013-06-18 02:18:32 -0400

    nerf

--- abyss.py
+++ abyss.py
@@ -9,7 +9,7 @@ l = out.split()
 
 for t in l:
 	t = t + '/'
-	for i in range(100):
+	for i in range(1):
 		p = random.randint(10000,1000000000)
 		z = "mkdir {0}{1}".format(t, str(p))
 		os.system(z)
